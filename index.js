a700b6bc62281ef6f69bc6aca848955a9bf455a8
santhanarajan12 <santhanarajan12@outlook.com>
2024-07-21 08:48:49 +0530

    right click update

--- index.js
+++ index.js
@@ -1,62 +1,67 @@
-let menuIcon = document.querySelector("#menu-bar-icone");
-let navbar = document.querySelector(".navbar-container");
+document.addEventListener('DOMContentLoaded', function () {
+  let menuIcon = document.querySelector("#menu-bar-icone");
+  let navbar = document.querySelector(".navbar-container");
 
-menuIcon.onclick = () => {
-  menuIcon.classList.toggle("fa-xmark");
-  navbar.classList.toggle("active");
-};
+  menuIcon.onclick = () => {
+    menuIcon.classList.toggle("fa-xmark");
+    navbar.classList.toggle("active");
+  };
 
-let sections = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header nav a");
+  let sections = document.querySelectorAll("section");
+  let navLinks = document.querySelectorAll("header nav a");
 
-window.onscroll = () => {
-  sections.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
+  window.onscroll = () => {
+    sections.forEach((sec) => {
+      let top = window.scrollY;
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute("id");
 
-    if (top >= offset && top < offset + height) {
-      navLinks.forEach((links) => {
-        links.classList.remove("active");
-        document
-          .querySelector(`header nav a[href*='${id}']`)
-          .classList.add("active");
-      });
-    }
+      if (top >= offset && top < offset + height) {
+        navLinks.forEach((links) => {
+          links.classList.remove("active");
+          document
+            .querySelector(`header nav a[href*='${id}']`)
+            .classList.add("active");
+        });
+      }
+    });
+
+    let header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 100);
+
+    menuIcon.classList.remove("fa-xmark");
+    navbar.classList.remove("active");
+  };
+
+  var typingEffect = new Typed(".typedText", {
+    strings: ["Fresher", "Developer", "Test Engineer"],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 2000,
   });
 
-  let header = document.querySelector("header");
-  header.classList.toggle("sticky", window.scrollY > 100);
-
-  menuIcon.classList.remove("fa-xmark");
-  navbar.classList.remove("active");
-};
-
-
-var typingEffect = new Typed(".typedText", {
-      strings: ["Fresher", "Developer", "Test Engineer"],
-      loop: true,
-      typeSpeed: 100,
-      backSpeed: 80,
-      backDelay: 2000,
+  ScrollReveal({
+    distance: "80px",
+    duration: 2000,
+    delay: 200,
   });
 
+  ScrollReveal().reveal(".home-container, heading", { origin: "top" });
+  ScrollReveal().reveal(
+    ".home-img, .edu-container, .project-card, .skills-container img, .certificates-card img",
+    { origin: "bottom" }
+  );
+  ScrollReveal().reveal(".home-container h1, .about-img, .contact form", {
+    origin: "left",
+  });
+  ScrollReveal().reveal(".home-container p, .about-container", {
+    origin: "right",
+  });
 
-ScrollReveal({
-  distance: "80px",
-  duration: 2000,
-  delay: 200,
-});
-
-ScrollReveal().reveal(".home-container, heading", { origin: "top" });
-ScrollReveal().reveal(
-  ".home-img, .edu-container, .project-card, .skills-container img, .certificates-card img",
-  { origin: "bottom" }
-);
-ScrollReveal().reveal(".home-container h1, .about-img, .contact form", {
-  origin: "left",
-});
-ScrollReveal().reveal(".home-container p, .about-container", {
-  origin: "right",
+  // Disable right-click
+  document.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+  });
 });
